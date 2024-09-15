610163a1fff6f07738f8af7c137475ec94414259
Patrick <patrickjreid@gmail.com>
2024-09-15 15:49:00 -0600

    app.vue file matches playground.html

--- sandbox/src/App.vue
+++ sandbox/src/App.vue
@@ -1,47 +1,65 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script >
+  export default {
+    data() {
+      return {
+        // These are interactive data properties:
+        count: 0,
+        counterTitle: 'Counter Standard',
+        incrementAmount: 8,
+        message: 'it works!',
+        listOfNumbers: [1,2,3,4,5]
+        }
+    },
+    computed:{
+      displayTitle() {
+        if (this.count < 20) {
+          return "Small Number title"
+        } else {
+          return 'Large number title'
+        }
+      }
+    },
+    methods: {
+      incrementCount() {
+        this.count += this.incrementAmount
+      },
+    },
+        // watch: {
+          
+        //   count(newValue) {
+        //     // the normal paradigm:
+        //     if(newValue > 20) {
+        //       this.counterTitle += " Very long"
+        //     }
+        // // But using compute lets vue deturmine when its dependancies have changed. 
+        //     console.log(newValue)
+        //   }
+        // }
+      
+  }
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div >
+    <p>hello world</p>
+      <h1>{{ displayTitle}}</h1>
+      <p>{{ count }}</p>
+      <button v-on:click="incrementCount">incrememnt count</button>
+      <div>
+        <label for="incrementAmount">increment by:</label>
+        <input 
+          type="number" 
+          v-model="incrementAmount"
+        >
+      </div>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <hr/>
+      <p v-if="message.length % 2 === 0">Even: {{message}} </p>
+      <p v-else>Odd: {{message.toUpperCase()}} </p>
+      <ul>
+        <li v-for="(number, index) in listOfNumbers" :key="`item-${index}`">{{number}}</li>
+      </ul>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
