477c0640851a9381aa8dfd227aa9179c0ea29002
Patrick <patrickjreid@gmail.com>
2024-09-16 11:42:34 -0600

    figuing out some bugs

--- sandbox/src/App.vue
+++ sandbox/src/App.vue
@@ -1,65 +1,65 @@
-<script >
-  export default {
-    data() {
-      return {
-        // These are interactive data properties:
-        count: 0,
-        counterTitle: 'Counter Standard',
-        incrementAmount: 8,
-        message: 'it works!',
-        listOfNumbers: [1,2,3,4,5]
-        }
-    },
-    computed:{
-      displayTitle() {
-        if (this.count < 20) {
-          return "Small Number title"
-        } else {
-          return 'Large number title'
-        }
-      }
-    },
-    methods: {
-      incrementCount() {
-        this.count += this.incrementAmount
-      },
-    },
-        // watch: {
-          
-        //   count(newValue) {
-        //     // the normal paradigm:
-        //     if(newValue > 20) {
-        //       this.counterTitle += " Very long"
-        //     }
-        // // But using compute lets vue deturmine when its dependancies have changed. 
-        //     console.log(newValue)
-        //   }
-        // }
-      
+<script lang="js">
+// import Counter from './components/Counter-item.vue';
+import Counter from "./components/Counter-item.vue"
+export default {
+  components: {
+    Counter,
   }
+  // data() {
+  //   return {
+  //     // These are interactive data properties:
+  //     count: 0,
+  //     counterTitle: 'Counter Standard',
+  //     incrementAmount: 1,
+  //     message: 'it works!',
+  //     listOfNumbers: [1, 2, 3, 4, 5]
+  //   }
+  // },
+  // computed: {
+  //   displayTitle() {
+  //     if (this.count < 20) {
+  //       return 'Small Number title'
+  //     } else {
+  //       return 'Large number title'
+  //     }
+  //   }
+  // },
+  // methods: {
+  //   incrementCount() {
+  //     this.count += this.incrementAmount
+  //   }
+  // }
+  // watch: {
+
+  //   count(newValue) {
+  //     // the normal paradigm:
+  //     if(newValue > 20) {
+  //       this.counterTitle += " Very long"
+  //     }
+  // // But using compute lets vue deturmine when its dependancies have changed.
+  //     console.log(newValue)
+  //   }
+  // }
+}
 </script>
 
 <template>
-  <div >
-    <p>hello world</p>
-      <h1>{{ displayTitle}}</h1>
-      <p>{{ count }}</p>
-      <button v-on:click="incrementCount">incrememnt count</button>
-      <div>
-        <label for="incrementAmount">increment by:</label>
-        <input 
-          type="number" 
-          v-model="incrementAmount"
-        >
-      </div>
-
-      <hr/>
-      <p v-if="message.length % 2 === 0">Even: {{message}} </p>
-      <p v-else>Odd: {{message.toUpperCase()}} </p>
-      <ul>
-        <li v-for="(number, index) in listOfNumbers" :key="`item-${index}`">{{number}}</li>
-      </ul>
+  <p>App.vue</p>
+  <Counter />
+  <!-- <div> -->
+    <!-- <h1>{{ displayTitle }}</h1>
+    <p>{{ count }}</p>
+    <button v-on:click="incrementCount">incrememnt count</button>
+    <div>
+      <label for="incrementAmount">increment by:</label>
+      <input type="number" v-model="incrementAmount" />
     </div>
+
+    <hr />
+    <p v-if="message.length % 2 === 0">Even: {{ message }}</p>
+    <p v-else>Odd: {{ message.toUpperCase() }}</p>
+    <ul>
+      <li v-for="(number, index) in listOfNumbers" :key="`item-${index}`">{{ number }}</li>
+    </ul> -->
+  <!-- </div> -->
 </template>
-
-
---
+++ sandbox/src/components/Counter-item.vue
@@ -0,0 +1,53 @@
+<script>
+export default {
+    // This is basically like initial state in redux. 
+    data: () => ({
+        count: 0,
+        counterTitle: 'Counter Standard',
+        incrementAmount: 8,
+    }),
+    // computed cannot have side effects (like redux), if it has a side effect it can run a method or watch is a function based on an event or a change that can have a side effect. 
+    computed:{
+      displayTitle() {
+        if (this.count < 20) {
+          return "Small Number title"
+        } else {
+          return 'Large number title'
+        }
+      },
+      optimisedIncrementalAmount() {
+        return this.displayTitle.length * this.incrementAmount
+      }
+    },
+    // watch is a function based on an event that can have side effects. 
+    // methods are functions 
+    methods: {
+      incrementCount() {
+        this.count += this.incrementAmount
+      },
+    },
+}
+</script>
+
+<template>
+    <div >
+      <p>Counter-item</p>
+        <h1>{{ displayTitle}}</h1>
+        <p>{{ count }}</p>
+        <button v-on:click="incrementCount">incrememnt count</button>
+        <div>
+          <label for="incrementAmount">increment by:</label>
+          <input 
+            type="number" 
+            v-model="incrementAmount"
+          >
+        </div>
+  
+        <hr/>
+        <p v-if="message.length % 2 === 0">Even: {{message}} </p>
+        <p v-else>Odd: {{message.toUpperCase()}} </p>
+        <ul>
+          <li v-for="(number, index) in listOfNumbers" :key="`item-${index}`">{{number}}</li>
+        </ul>
+      </div>
+  </template>
